6a2cf5dea584782610b7b54f55eb749e4475cc5b
Lucio Benini <iiriya@iiriya.com>
2020-05-10 11:10:25 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,16 +12,12 @@ export default class AboutPage extends React.Component {
       <IndexLayout location={this.props.location}>
         <Section title="Chi Sono" subtitle="Anna Zattoni">
           <div className="columns">
+            <div className="column is-one-thirds"><img src={image} alt="Anna Zattoni" title="Anna Zattoni" /></div>
             <div className="column is-two-thirds">
-              Nel 2003, a pochi mesi dall'esame di maturità, ho riflettuto su una cosa: come si potevano conoscere opere come la Divina Commedia o l'Odissea, l'arte di Dalì o quella di Monet, i grandi classici della letteratura inglese, francese e americana, e non avere la benché minima idea del perché un fiore fosse colorato?<br />
+              <p>Nel 2003, a pochi mesi dall'esame di maturità, ho riflettuto su una cosa: come si potevano conoscere opere come la Divina Commedia o l'Odissea, l'arte di Dalì o quella di Monet, i grandi classici della letteratura inglese, francese e americana, e non avere la benché minima idea del perché un fiore fosse colorato?<br />
 Un bel giorno, mi sono chiesta: "Perché piove?", "Perché alcuni animali migrano?", "Perché ci sono zone in cui la Terra trema?".<br />
 Desideravo conoscere e capire gli equilibri ecosistemici per conoscere e capire a fondo la Terra.<br />
-Ecco perché ho scelto di laurearmi in Scienze Naturali e in Ecologia Applicata.
-</div>
-            <div className="column is-one-thirds"><img src={image} alt="Anna Zattoni" title="Anna Zattoni" /></div>
-          </div>
-          <div className="columns">
-            <div className="column is-three-thirds">
+Ecco perché ho scelto di laurearmi in Scienze Naturali e in Ecologia Applicata.</p>
               <p>Oggi sono una copywriter appassionata di sostenibilità ambientale che, attraverso MyHumus, racconta il territorio italiano esplorandolo attraverso uno sguardo "green".</p>
             </div>
           </div>
