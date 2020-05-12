e0083fb29772106c4f4e5f01f550223aa96b0707
annazattoni <63807607+annazattoni@users.noreply.github.com>
2020-05-12 14:18:40 +0200

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,11 +14,11 @@ export default class AboutPage extends React.Component {
           <div className="columns">
             <div className="column is-one-thirds"><img src={image} alt="Anna Zattoni" title="Anna Zattoni" /></div>
             <div className="column is-two-thirds">
-              <p>Nel 2003, a pochi mesi dall'esame di maturità, ho riflettuto su una cosa: come si potevano conoscere opere come la Divina Commedia o l'Odissea, l'arte di Dalì o quella di Monet, i grandi classici della letteratura inglese, francese e americana, e non avere la benché minima idea del perché un fiore fosse colorato?<br />
-Un bel giorno, mi sono chiesta: "Perché piove?", "Perché alcuni animali migrano?", "Perché ci sono zone in cui la Terra trema?".<br />
-Desideravo conoscere e capire gli equilibri ecosistemici per conoscere e capire a fondo la Terra.<br />
+              <p>Nel 2003, a pochi mesi dall'esame di maturità, ho riflettuto su una cosa: come si potevano conoscere opere come la Divina Commedia o l'Odissea, l'arte di Dalì o quella di Monet, i grandi classici della letteratura inglese, francese e americana, senza avere la benché minima idea del perché un fiore fosse colorato?<br />
+Un bel giorno, mi sono chiesta: "Perché piove?", "Perché alcuni animali migrano e altri no?", "Perché in alcune zone la Terra trema?".<br />
+Desideravo conoscere e comprendere gli equilibri ecosistemici per conoscere e comprendere a fondo la Terra.<br />
 Ecco perché ho scelto di laurearmi in Scienze Naturali e in Ecologia Applicata.</p>
-              <p>Oggi sono una copywriter appassionata di sostenibilità ambientale che, attraverso MyHumus, racconta il territorio italiano esplorandolo attraverso uno sguardo "green".</p>
+              <p>Oggi sono una copywriter appassionata di sostenibilità ambientale che, grazie a MyHumus, racconta il territorio italiano esplorandolo attraverso uno sguardo "green".</p>
             </div>
           </div>
         </Section>
@@ -28,10 +28,10 @@ Ecco perché ho scelto di laurearmi in Scienze Naturali e in Ecologia Applicata.
           </p>
           <p>
             MyHumus non è solo un blog che parla di sostenibilità ambientale: è un viaggio avventuroso tra persone e progetti.<br />
-Come l'humus del terreno sprigiona tutta la sua essenzialità alla sopravvivenza dell'ecosistema, le persone che intervisto rappresentano una componente indispensabile del nostro territorio: ci sono, magari stanno compiendo qualcosa di singolare e di importante, eppure sono in pochi a saperlo.
+Come l'humus del terreno sprigiona tutta la sua essenzialità alla sopravvivenza dell'ecosistema, le persone che intervisto rappresentano una componente indispensabile del nostro territorio: ci sono, stanno compiendo qualcosa di singolare e di importante, ma non tutti lo sanno.
 </p>
-          <p>MyHumus racconta di passioni e di creatività, d'innovazione e di visioni lungimiranti. Ed è la dimostrazione di quanti cuori pulsanti si celino in Italia, uniti da un sottile filo di colore verde: l'ambiente.</p>
-          <p>Nel 2014, MyHumus è stato incluso fra i progetti innovativi di Progetto Manifattura, hub italiano dedicato unicamente alla green innovation.</p>
+          <p>MyHumus racconta di passioni e di creatività, d'innovazione e di visioni lungimiranti. Ed è la dimostrazione di quanti cuori pulsanti ci siano in Italia, uniti da un sottile filo di colore verde: l'ambiente.</p>
+          <p>Nel 2014, MyHumus è stato incluso fra i progetti innovativi di [Progetto Manifattura](https://progettomanifattura.it), hub italiano dedicato unicamente alla green innovation.</p>
         </Section>
       </IndexLayout>
     )
